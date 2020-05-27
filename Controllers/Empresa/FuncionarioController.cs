5709f71578ea314829e28fdd231728d3565a7a9a
Verners Augusto <verners_sccp@hotmail.com>
2020-05-26 21:01:16 -0300

    Adicionado controller e views do funcionario

--- Controllers/Empresa/FuncionarioController.cs
+++ Controllers/Empresa/FuncionarioController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Marketplace.Models.Entidades;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Marketplace.Controllers.Empresa
@@ -7,7 +9,40 @@ namespace Marketplace.Controllers.Empresa
         public IActionResult Index()
         {
             ViewBag.Title = "Funcionários";
-            return View("~/Views/Empresa/Funcionario/Index.cshtml");
-        }
+            List<Funcionario> lista = new List<Funcionario>();   
+            Funcionario funcionario = new Funcionario();
+            lista.Add(funcionario);
+
+            return View(lista);
+        }      
+        
+        [HttpGet]
+        public IActionResult Create()
+        {           
+            return View("~/Views/Funcionario/Create.cshtml");
+        }      
+
+        [HttpPost]
+        public IActionResult Create(Funcionario model)
+        {         
+            //TODO   
+            return RedirectToAction("Create");
+        }      
+        [HttpGet]
+        public IActionResult Update(string id)
+        {           
+            return View(new Funcionario());
+        }      
+
+        [HttpPost]
+        public IActionResult Update(string id, Funcionario model)
+        {         
+            //TODO   
+            return RedirectToAction("/");
+        }      
+
+        
+
+          
     }
 }
