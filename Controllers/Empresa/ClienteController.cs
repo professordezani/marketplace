1c01d6f232367f93c1976da39bbde234b747f512
Verners Augusto <verners_sccp@hotmail.com>
2020-06-03 21:41:52 -0300

    adicionado hash da senha do usuário

--- Controllers/Empresa/ClienteController.cs
+++ Controllers/Empresa/ClienteController.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Security.Cryptography;
 using Marketplace.Models.Entidades;
 using Marketplace.Models.Repositories;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Marketplace.Controllers.Empresa
@@ -44,6 +46,23 @@ namespace Marketplace.Controllers.Empresa
                 return View(model);
 
             model.Id = Guid.NewGuid();
+
+            var password = model.Senha;
+
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA1,
+            iterationCount: 10000,
+            numBytesRequested: 256 / 8));
+
+            model.Senha = hashed;            
 
             _clienteRepository.Create(model);
 
--- Models/Entidades/Cliente.cs
+++ Models/Entidades/Cliente.cs
@@ -5,12 +5,12 @@ namespace Marketplace.Models.Entidades
    public class Cliente : Usuario
    {
        [Required(ErrorMessage="CPF é obrigatório")]
-       [MaxLength(9)]
-       [MinLength(9)]
+       [MaxLength(11, ErrorMessage="CPF deve ter 11 Caracteres")]
+       [MinLength(11, ErrorMessage="CPF deve ter 11 Caracteres")]
        public string Cpf { get; set; }
        
        [Required(ErrorMessage="Endereço é obrigatório")]
-       [MaxLength(200)]
+       [MaxLength(200, ErrorMessage="Endereço deve conter no máximo 200 caracteres")]
        public string Endereco { get; set; }
 
    }
